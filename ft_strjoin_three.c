6065ec632c8aeac51b10ef185487b5333f662817
Kristina Nemcova <knemcova@c4r2s17.42tokyo.jp>
2025-04-09 13:37:10 +0900

    norm check

--- ft_strjoin_three.c
+++ ft_strjoin_three.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin_three.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oyuhi <oyuhi@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*   By: knemcova <knemcova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/16 10:38:00 by oyuhi             #+#    #+#             */
-/*   Updated: 2025/03/29 18:27:08 by oyuhi            ###   ########.fr       */
+/*   Updated: 2025/04/09 13:36:03 by knemcova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin_three(const char *str1,const  char *str2,const  char *str3)
+char	*ft_strjoin_three(const char *str1, const char *str2, const char *str3)
 {
 	char	*tmp;
 	char	*result;
